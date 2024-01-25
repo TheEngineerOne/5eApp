8421230656c7460daa50d01ec9b9c3200d406a4d
TheEngineerOne <verbruggenlucas2@gmail.com>
2024-01-25 16:07:05 +0100

    adding RangeType

--- app/src/main/kotlin/_5eApp/App.kt
+++ app/src/main/kotlin/_5eApp/App.kt
@@ -2,4 +2,6 @@ package _5eApp
 
 fun main(arg : Array<String>) {
     println("Hello, World!\n")
+    var distanceType:DistanceType = DistanceType.SELF
+    println(distanceType)
 }
--- app/src/main/kotlin/_5eApp/Range.kt
+++ app/src/main/kotlin/_5eApp/Range.kt
@@ -1,3 +1,39 @@
 package _5eApp
 
+enum class RangeType{
+    POINT{
+        override fun toString():String{
+            return "point"
+        }
+    },
+    RADIUS{
+        override fun toString():String{
+            return "radius"
+        }
+    };
+}
+
+enum class DistanceType{
+    SELF{
+        override fun toString():String{
+            return "self"
+        }
+    },
+    TOUCH{
+        override fun toString():String{
+            return "touch"
+        }
+    },
+    FEET{
+        override fun toString():String{
+            return "feet"
+        }
+    },
+    SIGHT{
+        override fun toString():String{
+            return "sight"
+        }
+    };
+}
+
 data class Range(val type:String)
